9efd124d22c16cbdde10e08615b3101b0d342d17
rsoome <rsoome16@gmail.com>
2018-06-13 02:15:50 +0300

    menu almost working

--- main.cpp
+++ main.cpp
@@ -12,12 +12,11 @@
 #include <ctype.h>
 #include <string.h>
 
-
 //void sendCommand(String cmd){
   
 //}
 
-void refreshScreen(char *lines[]);
+void refreshScreen(char**, int);
 
 char *replace_char(char* str, int pos, char replace){
     char buf[16];
@@ -41,116 +40,103 @@ char *replace_char(char* str, int pos, char replace){
     }
 }
 
-struct linkedListS { //Doubly linked list for strings
-
-  char *value;
-  int *size;
-  int *id;
-  struct linkedListS *parent;
-  struct linkedListS *child;
-};
-
-char **llsToLiteralArr(struct linkedListS *lls, int start, int end){
-  char **literalArr = malloc(end - start + 1);
-  
-  struct linkedListS *current = lls;
-  char *buf[2];
-  while(*((*current).id) != start){
-    if((*current).child == NULL) break;
-    sprintf(buf[0], "%d", (*current).id);
-    buf[1] = NULL;
-    refreshScreen(buf);
-    current = (*current).child;
-  }
-  
-  int i = 0;
-  while((*current).child != NULL && i < end - start){
-    literalArr[i] = (*current).value;
-    current = (*current).child;
-    i++;
-  }
-  literalArr[i] = NULL;
-  return literalArr;
-}
-
-int appendTolls(struct linkedListS *lls, char *s){
-  *((*lls).size)++;
-  struct linkedListS *current = lls;
-  while((*current).child != NULL) 
-  {
-    (*current).size = (*lls).size;
-    current = (*current).child;
-  }
-  struct linkedListS *newLink = malloc(sizeof(struct linkedListS));
-  if(newLink == NULL) return 0;
-  (*newLink).value = s;
-  (*newLink).size = (*lls).size;
-  int *id = malloc(sizeof(int));
-  if(id == NULL) return 0;
-  memcpy(&((*newLink).size), id, sizeof(int));
-  *id = *((*newLink).size) - 1;
-  (*newLink).id = id;
-  (*newLink).parent = current;
-  (*newLink).child = NULL;
-  (*current).child = newLink;
-  return 1;
-  
-}
-
-struct linkedListS *arrTolls(char **arr){
-  struct linkedListS *lls = malloc(sizeof(struct linkedListS));
-  (*lls).value = NULL;
-  int *id = malloc(sizeof(int));
-  *id = 0;
-  (*lls).id = id;
-  int *size = malloc(sizeof(int));
-  *size = 0;
-  (*lls).size = size;
-  (*lls).child = NULL;
-  (*lls).parent = NULL;
-  for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++){
-    if(!appendTolls(lls, arr[i])){
-      char buf[35];
-      sprintf(buf, "%s%d%s", "malloc failed in appendTolls at: ", i, '\0');
-      char *error[2];
-      error[0] = buf;
-      error[1] = NULL;
-      refreshScreen(error);
-      while(1);
-    }
-  }
-  return lls;
-}
 
 struct profile {
   char **name;
-  struct linkedListS *commands; //Contains the commands
-};
-
-struct linkedListP {//Doubly linked list for profiles
-  struct profile *prfl;
-  int size;
-  int id ;
-  struct linkedListP *parent;
-  struct linkedListP *child;
+  char *cmdMenu[128];
 };
 
 struct menu {
   int pointer_pos;
   int currentMenu;
   
-  struct linkedListP *profiles;
+  struct profile **profiles;
   
-  struct linkedListS *mainMenu;
-  struct linkedListS *profMenu;
-  struct linkedListS *profInterMenu;
-  struct linkedListS *cmdMenu;
-  struct linkedListS *newCmdStart;
-  struct linkedListS *newCmdSave;
-  
-  struct linkedListS *current;
+  char **mainMenu;
+  char **profMenu;
+  char **profInterMenu;
+  char **cmdMenu;
+  char **newCmdStart;
+  char **newCmdSave;
   
 };
+
+char **getMenu(struct menu *m){
+
+  switch(m->currentMenu){
+    case 0:
+      return m->mainMenu;
+    case 1:
+      return m->profMenu;
+    case 2:
+      return m->profInterMenu;
+    case 3:
+      return m->cmdMenu;
+    case 4:
+      return m->newCmdStart;
+    case 5:
+      return m->newCmdSave;
+  }
+}
+
+void updateMenuPointer(struct menu *m, int dir){
+  int newPos = m->pointer_pos + dir;
+  if(newPos > -1 && getMenu(m)[m->currentMenu][m->pointer_pos] != NULL){
+    m->pointer_pos = newPos;
+  }
+}
+
+void changeMenu(struct menu *m){
+  /*
+    0: main menu
+    1: profile menu
+    2: profile intermediate menu
+    3: cmd menu
+    4: add new cmd
+    5: save cmd
+  */
+  
+  switch((*m).currentMenu){
+    
+    case 0:
+      switch(m->pointer_pos){
+        case 0:
+          m->currentMenu = 1;
+          break;
+        case 1:
+          //addProfile((*m).profiles);
+          break;
+      }
+      break;
+      
+    case 1:
+      //get profile
+      m->currentMenu = 2;
+      break;
+      
+    case 2:
+      switch(m->pointer_pos){
+        case 0:
+          //cmd menu
+          break;
+        case 1:
+          m->currentMenu = 4;
+          break;
+      }
+      break;
+    case 3:
+      //cmdMenu
+      m->currentMenu = 2;
+      break;
+    case 4:
+      m->currentMenu = 5;
+      break;
+    case 5:
+      m->currentMenu = 2;
+      break;
+  }
+  m->pointer_pos = 0;
+}
 
 struct keyboard {
   int selectedLine;
@@ -161,14 +147,14 @@ struct keyboard {
   
 };
 
-void refreshScreen(char *lines[]){
+void refreshScreen(char **lines, int pointer){
   RIT128x96x4Clear();
-  
+  if(pointer > -1) RIT128x96x4StringDraw (">", 0, 5 + pointer * 10, 3);
   int offset = 0;
   int i = 0;
   char *line = lines[0];
   while(line != NULL){
-    RIT128x96x4StringDraw (line, 5, 5 + offset, 3);
+    RIT128x96x4StringDraw (line, 10, 5 + offset, 3);
     offset += 10;
     i++;
     line = lines[i];
@@ -233,7 +219,7 @@ char* userInput(){
   char *ui[6];
   ui[0] = "";
   buldUI(kb.lines, 1, &ui);
-  refreshScreen(ui);
+  refreshScreen(ui, -1);
   while(1){
     
     btnUpState = GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_0) & GPIO_PIN_0;
@@ -247,29 +233,29 @@ char* userInput(){
     if((btnDownLastState != 0 && btnDownState == 0)){
       nextLine(&kb, 1);
       buldUI(kb.lines, 1, &ui);
-      refreshScreen(ui);
+      refreshScreen(ui, -1);
     }
     if(btnUpLastState != 0 && btnUpState == 0){
       nextLine(&kb, -1);
       buldUI(kb.lines, 1, &ui);
-      refreshScreen(ui);
+      refreshScreen(ui, -1);
     }
     if(btnLeftLastState != 0 && btnLeftState == 0){
       nextChar(&kb, -1);
       buldUI(kb.lines, 1, &ui);
-      refreshScreen(ui);
+      refreshScreen(ui, -1);
     }
     if(btnRightLastState != 0 && btnRightState == 0){
       nextChar(&kb, 1);
       buldUI(kb.lines, 1, &ui);
-      refreshScreen(ui);
+      refreshScreen(ui, -1);
     }
     if(btnSelectLastState != 0 && btnSelectState == 0){
       input[pos] = kb.lines[kb.selectedLine][kb.selected];
       pos++;
       input[pos] = '\0';
       ui[0] = input;
-      refreshScreen(ui);
+      refreshScreen(ui, -1);
       if(pos >= 17){
         break;
       }
@@ -292,54 +278,9 @@ struct profile *createProfile(){
   char temp[16];
   char *prfName[16] = malloc(sizeOf(temp));
   *prfName = userInput();
-  (*prfl).name = prfName;
+  prfl->name = prfName;
   
   return prfl;
-}
-
-void addProfile(struct linkedListP *p){
-  struct profile *prfl = createProfile();
-  (*p).size = (*p).size + 1;
-  struct linkedListP *current = p;
-  while((*current).child != NULL){
-    current = (*p).child;
-    (*current).size = (*p).size;
-  }
-  struct linkedListP newP;
-  newP.prfl = prfl;
-  newP.size = (*p).size;
-  newP.id = newP.size - 1;
-  newP.parent = current;
-  (*current).child = &newP;
-  
-}
-
-void changeMenu(struct menu *m){
-  /*
-    0: main menu
-    1: profile menu
-    2: profile intermediate menu
-    3: cmd menu
-    5: add new cmd
-    6: save cmd
-  */
-  
-  switch((*m).currentMenu){
-    
-    case 0:
-      switch((*m).pointer_pos){
-        case 0:
-          (*m).currentMenu = 1;
-          break;
-        case 1:
-          addProfile((*m).profiles);
-          break;
-      }
-      break;
-      
-    case 1:
-      
-  }
 }
 
 int main()
@@ -356,21 +297,29 @@ int main()
   GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_1);
   GPIOPadConfigSet(GPIO_PORTF_BASE,GPIO_PIN_1, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
   
-  struct linkedListP *profiles;
+  struct profile *profiles;
   
-  char *mainMenuOptions[2] = {"Select profile", "New profile"};
-  struct linkedListS *mainMenu = arrTolls(mainMenuOptions);
-  struct linkedListS *profMenu;
-  char *profInterOptions[2] = {"Select command", "New command"};
-  struct linkedListS *profInterMenu = arrTolls(profInterOptions);
-  struct linkedListS *cmdMenu;
-  char *newCmdStartOptions[1] = {"Start"};
-  struct linkedListS *newCmdStart = arrTolls(newCmdStartOptions);
-  char *newCmdSaveOptions[1] = {"Save"};
-  struct linkedListS *newCmdSave = arrTolls(newCmdSaveOptions);
-  
-  
-  struct menu m = {0, 0, profiles, mainMenu, profMenu, profInterMenu, cmdMenu, newCmdStart, newCmdSave};
+  char *mainMenu[3] = {"Select profile", "New profile", NULL};
+  char *profInterMenu[3] = {"Select command", "New command", NULL};
+  char *profMenu[19];
+  profMenu[0] = "";
+  profMenu[1] = NULL;
+  char *cmdMenu[129];
+  cmdMenu[0] = "";
+  cmdMenu[1] = NULL;
+  char *newCmdStart[2] = {"Start", NULL};
+  char *newCmdSave[2] = {"Save", NULL};
+ 
+  struct menu m;
+  m.currentMenu = 0;
+  m.pointer_pos = 0;
+  m.profiles = &profiles;
+  m.mainMenu = mainMenu;
+  m.profInterMenu = profInterMenu;
+  m.profMenu = profMenu;
+  m.cmdMenu = cmdMenu;
+  m.newCmdStart = newCmdStart;
+  m.newCmdSave = newCmdSave;
   
   int btnUpLastState = 0;
   int btnUpState = 0;
@@ -380,22 +329,41 @@ int main()
   int btnRightState = 0;
   int btnLeftLastState = 0;
   int btnLeftState = 0;
+  int btnSelectLastState = 0;
+  int btnSelectState = 0;
   
-  char *menuUI[4];
+  char *menuUI[129];
   while(1){
     
     btnUpState = GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_0) & GPIO_PIN_0;
     btnDownState = GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_1) & GPIO_PIN_1 ;
     btnLeftState = GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_2) &GPIO_PIN_2;
     btnRightState = GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_3) & GPIO_PIN_3;
+    btnSelectState = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_1) & GPIO_PIN_1;
     
-    char **elements = llsToLiteralArr(m.current, 0, 3);
-    buildUI(elements, 0, &menuUI);
-    refreshScreen(menuUI);
-    for(int i = 0; i < 10000000; i++){}
+    if(!btnDownLastState && btnDownState){
+      updateMenuPointer(&m, 1);
+      char **elements = getMenu(&m);
+      buildUI(elements, 0, &menuUI);
+      refreshScreen(menuUI, m.pointer_pos);
+    }
+    if(!btnUpLastState && btnUpState){
+      updateMenuPointer(&m, -1);
+      char **elements = getMenu(&m);
+      buildUI(elements, 0, &menuUI);
+      refreshScreen(menuUI, m.pointer_pos);
+    }
+    if(btnSelectLastState && !btnSelectState){
+      changeMenu(&m);
+      char **elements = getMenu(&m);
+      buildUI(elements, 0, &menuUI);
+      refreshScreen(menuUI, m.pointer_pos);
+    }
+    
     btnUpLastState = btnUpState;
     btnDownLastState = btnDownState;
     btnLeftLastState = btnLeftState;
     btnRightLastState = btnRightState;
+    btnSelectLastState = btnSelectState;
   }
 }
--- settings/miniprojekt.Debug.cspy.ps1
+++ settings/miniprojekt.Debug.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\Ramus\Documents\miniprojekt\settings\miniprojekt.Debug.general.xcl" --backend -f "C:\Users\Ramus\Documents\miniprojekt\settings\miniprojekt.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\Janar\Documents\miniprojekt\settings\miniprojekt.Debug.general.xcl" --backend -f "C:\Users\Janar\Documents\miniprojekt\settings\miniprojekt.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\Ramus\Documents\miniprojekt\settings\miniprojekt.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\Ramus\Documents\miniprojekt\settings\miniprojekt.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\Janar\Documents\miniprojekt\settings\miniprojekt.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\Janar\Documents\miniprojekt\settings\miniprojekt.Debug.driver.xcl" 
 }
